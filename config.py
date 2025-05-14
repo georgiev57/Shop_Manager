02b2f663dc0590009f98698073cd56000520e73e
georgiev57 <64395654+georgiev57@users.noreply.github.com>
2025-05-14 21:52:00 +0300

    DATABASE_URL

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
 
+url = os.getenv("DATABASE_URL")
+if url and url.startswith("postgres://"):
+    url = url.replace("postgres://", "postgresql://", 1)
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = url
     SQLALCHEMY_TRACK_MODIFICATIONS = False
